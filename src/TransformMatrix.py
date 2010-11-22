b25e54b366e890d6ad8a625d17fd46bc446a0164
Andrey Volkov <evil.bobby@gmail.com>
2010-11-22 12:34:16 +1000

    draft reverse func part 3, not tested

--- src/TransformMatrix.py
+++ src/TransformMatrix.py
@@ -8,8 +8,7 @@ class ReMatrix:
   """
   global ROWS
   global COLUMNS
-  global gSpecialDouble
-  global DoubleArray
+#  global gSpecialDouble
 #  global Amplitudes
   
   ROWS = 35     # количество строк
@@ -23,10 +22,13 @@ class ReMatrix:
     матрицы.
     """
     # Разбиваем список на 2 части, левую и правую.
-    
-    # Считаем амплитуды двух матриц
-    self.leftMatrix = self.CalcAmplitude(pervoMatrix)
-    self.rightMatrix = self.CalcAmplitude(pervoMatrix)
+    self.leftMatrix = pervoMatrix[:840]
+    self.rightMatrix = pervoMatrix[840:]
+    # Создаём массив амплитуд  
+    self.Amplitudes = [[None for x in xrange(ROWS)] for y in xrange(COLUMNS)] #@UnusedVariable
+    # Расчет амплитуд двух матриц
+    self.leftMatrix = self.CalcAmplitude(self.leftMatrix)
+    self.rightMatrix = self.CalcAmplitude(self.rightMatrix)
     # Переставляем значения матриц
     self.leftMatrix = self.ShuffleMatrix(self.leftMatrix)
     self.rightMatrix = self.ShuffleMatrix(self.rightMatrix)
@@ -38,27 +40,29 @@ class ReMatrix:
     Расстановка значений матрицы в правильном порядке
     """
     # Создаём результирующую матрицу
-    self.rezMatrix = range(ROWS)
-    for i in range(ROWS):
-      self.rezMatrix[i] = range(COLUMNS)
+    self.rezMatrix = [[None for x in xrange(ROWS)] for y in xrange(COLUMNS)] #@UnusedVariable
     # Размещаем значения normAmplMatrx в правильном порядке и заносим в rezMatrix
-    for j in range(ROWS, -1, -1):
-      for i in range(14, -1, -1):
-        pass
-      for i in range(COLUMNS, 14, -1):
-        pass
-    return self.rezMatrix
+    for j, k in zip(xrange(ROWS-1,-1,-1), xrange(ROWS)):
+      for i, m in zip(xrange(14,-1,-1), xrange(14)):
+#        self.rezMatrix[m][k] = normAmplMatrx[i][j]
+        print normAmplMatrx[i][j],
+      for i, m in zip(xrange(COLUMNS-1,13,-1), xrange(14, COLUMNS)):
+#        self.rezMatrix[m][k] = normAmplMatrx[i][j]
+        print normAmplMatrx[i][j],
+      print
+#    return self.rezMatrix
       
   
   def CalcAmplitude(self, pervoMatrix):
     """
     Расчет значений амплитуд сегментарной матрицы 
     """
-    for row in range(ROWS):
-      for col in range(COLUMNS):
+    self.gSpecialDouble = 0.0
+    for row in xrange(ROWS):
+      for col in xrange(COLUMNS):
         DataIndex = self.GetDataFromTable(row, col)
-        gSpecialDouble-=DataIndex
-        self.Amplitudes[row][col] = (float(pervoMatrix[DataIndex]) * (1.0 - gSpecialDouble) + float(pervoMatrix[DataIndex + 1]) * gSpecialDouble) * 0.001388888888888889   / DoubleArray[row] * (72.0 - col)
+        self.gSpecialDouble-=DataIndex
+        self.Amplitudes[row][col] = (float(pervoMatrix[DataIndex]) * (1.0 - self.gSpecialDouble) + float(pervoMatrix[DataIndex + 1]) * self.gSpecialDouble) * 0.001388888888888889 * (72.0 - col)
     return self.Amplitudes
   
   def GetDataFromTable(self, rowIndex, columnIndex):
@@ -66,9 +70,9 @@ class ReMatrix:
     22.70845, 22.061985, 21.433924, 20.823742, 20.23093, 19.654995, 19.095455, 18.551844, 
     18.023708, 17.510609, 17.012115, 16.527815, 16.057299, 15.60018, 15.156073, 14.724608, 
     14.305428, 13.89818)
-    gSpecialDouble = FloatTuple[columnIndex] / float(1 << (rowIndex / 5))
-    gSpecialDouble = gSpecialDouble - (gSpecialDouble * 0.5) * float(rowIndex % 5) * 0.2
-    gSpecialDouble = gSpecialDouble * 163.84
-    return long(gSpecialDouble)
+    self.gSpecialDouble = FloatTuple[columnIndex] / float(1 << (rowIndex / 5))
+    self.gSpecialDouble = self.gSpecialDouble - (self.gSpecialDouble * 0.5) * float(rowIndex % 5) * 0.2
+    self.gSpecialDouble = self.gSpecialDouble * 163.84
+    return long(self.gSpecialDouble)
   
   
